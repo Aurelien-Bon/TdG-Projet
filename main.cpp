6963d2c4f2f8b39839648c6ea0a1fef4dffcb2a4
Aurelien-Bon <78735114+Aurelien-Bon@users.noreply.github.com>
2021-04-07 11:07:04 +0200

    changement resolution

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ void Gotom(int col, int nb)
 int main()
 {
     std::cout<<"Ouvrez votre console en plein écran"<<std::endl;
-    system("mode con cols=1920 lines=1080");
+    system("mode con cols=200 lines=100");
     Station test("data_arcs.txt");
     //Station test("testprojet.txt");
     //test.Flotmin2pts();
@@ -20,23 +20,23 @@ int main()
     bool quitter=false;
     do
     {
-         Gotom(166,4);
+         Gotom(90,4);
         std::cout<<"Menu"<<std::endl;
-        Gotom(160,6);
+        Gotom(80,6);
         std::cout << "1. Choisir un trajet"<<std::endl;
-          Gotom(160,7);
+          Gotom(80,7);
         std::cout << "2. Choisir un lieu"<<std::endl;
-          Gotom(160,8);
+          Gotom(80,8);
         std::cout << "3. Choix d'un chemin entre 2 lieu"<<std::endl;
-          Gotom(160,9);
+          Gotom(80,9);
         std::cout << "4. Critere de selection des chemins"<<std::endl;
-          Gotom(160,10);
+          Gotom(80,10);
         std::cout << "5. Flow maximum entre 2 point"<<std::endl;
-          Gotom(160,11);
+          Gotom(80,11);
         std::cout << "6. Quitter"<<std::endl;
-          Gotom(160,12);
+          Gotom(80,12);
         std::cout << "Que voulez-vous faire?: ";
-         Gotom(160,13);
+         Gotom(80,13);
         int choix=0;
         do
         {
